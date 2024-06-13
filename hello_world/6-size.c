4fde7062aea9363f6f6a5041b6d95307aaf8f3cf
Swagtiago29 <sant29ramos@gmail.com>
2024-06-13 17:09:17 +0000

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *Description:  main - size of stuff
+ *Description: main - size of stuff
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,5 +11,4 @@ int main(void)
     printf("Size of char: %zu byte\n", sizeof(charType));
    
     return (0);
-
 }
