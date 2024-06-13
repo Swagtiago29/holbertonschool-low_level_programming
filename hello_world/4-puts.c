5483fedff63e26b189879bdd883da51d1ab03bd1
Swagtiago29 <sant29ramos@gmail.com>
2024-06-13 15:16:57 +0000

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main print programming is like building a multilinguial puzzle
+ * Description: main -  print programming is like building a multilinguial..
+ * Return: Always 0 (success)
  */
 int main(void)
 {
