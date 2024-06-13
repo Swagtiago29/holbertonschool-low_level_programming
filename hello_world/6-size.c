be5c52237a6863b4c27864cc4a53c5d0a3e3faef
Swagtiago29 <sant29ramos@gmail.com>
2024-06-13 17:27:03 +0000

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-   printf("Size of a char: %zu byte(s)\n", sizeof(char));
-   printf("Size of int: %zu byte(s)\n", sizeof(int));
-   printf("Size of longint: %zu byte(s)\n", sizeof(long int));
-   printf("Size of longlongint: %zu byte(s)\n", sizeof(long long int));
-   printf("Size of float: %zu byte(s)\n", sizeof(float));
-    return (0);
+   printf("Size of a char: %u byte(s)\n", sizeof(char));
+   printf("Size of int: %u byte(s)\n", sizeof(int));
+   printf("Size of long int: %u byte(s)\n", sizeof(long int));
+   printf("Size of long long int: %u byte(s)\n", sizeof(long long int));
+   printf("Size of float: %u byte(s)\n", sizeof(float));
+   return (0);
 }
