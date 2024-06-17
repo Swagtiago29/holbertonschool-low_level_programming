7ebe5d18bbf92a7ea72b8e93c5694112c2431f8b
Swagtiago29 <sant29ramos@gmail.com>
2024-06-17 16:11:41 +0000

    33

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0
+ * Description: this is a description
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	if (x < 9)
+		putchar(' ');
+		putchar(',');
+	}
+	return (0);
+}
