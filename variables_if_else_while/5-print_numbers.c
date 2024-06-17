59eebb1adac2fe2171dd6ae350e3c58994e07fc0
Swagtiago29 <sant29ramos@gmail.com>
2024-06-17 15:03:52 +0000

    aa

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0
+ * Description: this is a description
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+		putchar('\n');
+	return (0);
+}
