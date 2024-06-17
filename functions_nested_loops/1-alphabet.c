d0f2677ccca02254deb9d24c93907f1ef9dbc4ab
Swagtiago29 <sant29ramos@gmail.com>
2024-06-17 22:36:13 +0000

    oo

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,13 +5,12 @@
 *
 * Return: Always 0.
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	int (o);
-	while ((o = 97); o <= 122; o++)
-	{
-		_putchar(o);
-	}
+	char p;
+
+	for (p = 'a'; p <= 'z'; p++)
+		_putchar(p);
+
 	_putchar('\n');
-return (0);
 }
