5dc040b3b7848ba80043e180b428e9865a3e2a45
Swagtiago29 <sant29ramos@gmail.com>
2024-06-17 14:08:41 +0000

    com

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0
+ * Description: this is a description
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+	putchar(c);
+	putchar('\n');
+	}
+	return (0);
+}
