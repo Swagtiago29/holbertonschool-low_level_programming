8c00720064c97581c3c4c19b0e9cafcc46975a17
Swagtiago29 <sant29ramos@gmail.com>
2024-06-17 22:28:46 +0000

    oo

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
-* main - Entry point of my code
+* print_alphabet - Entry point of my code
 *
 * Return: Always 0.
 */
-int main(void)
+void print_alphabet(void);
 {
-
-	void print_alphabet(void);
+	int (o);
+	while ((o = 97); o <= 122; o++)
+	{
+		_putchar(o);
+	}
 	_putchar('\n');
 return (0);
 }
