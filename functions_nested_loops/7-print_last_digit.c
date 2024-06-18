edc766e64dc4fd4f776f4add22f39de4ba097ea4
Swagtiago29 <sant29ramos@gmail.com>
2024-06-18 13:42:50 +0000

    pp

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  _islower - Entry Point
+ * Return: Always 0
+ * Description: this is a description
+ * @c: also c
+ */
+int print_last_digit(int l)
+{
+	int lg = l%
+		_putchar(lg)
+		return (lg)
+}
