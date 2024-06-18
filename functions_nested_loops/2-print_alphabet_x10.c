961f246347ba1fe26aa277d37882eb551af74018
Swagtiago29 <sant29ramos@gmail.com>
2024-06-18 18:04:19 +0000

    ooo

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,16 @@
 *
 * Return: Always 0.
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char p;
 	int o;
 	for (o = 0; o <= 9; o++)
 	{
-	for (p == 'a'; p <= 'z'; p++)
-		_putchar(p);
-	if (p = 'z')
-                _putchar('\n');
+		for (p = 'a'; p <= 'z'; p++)
+			_putchar(p);
+		if (p == 'z')
+                	_putchar('\n');
 
-}
-	return (0);
+	}
 }
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _islower - Entry Point
+ * Return: Always 0
+ * Description: this is a description
+ * @c: also c
+ */
+void times_table(void)
+	int n
+	int n_2
+	int m
+{
+	for (n = 0; n <= 9; n++)
+		
+		for (n_2 = 0; n_2 <= 9; n_2++)
+				
+}
