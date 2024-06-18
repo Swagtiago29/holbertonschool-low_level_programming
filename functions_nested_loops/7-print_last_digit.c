ebea2359057603f68e1d085e8b7ab29e8951156e
Swagtiago29 <sant29ramos@gmail.com>
2024-06-18 14:43:03 +0000

    pp

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,5 +10,6 @@ int print_last_digit(int u)
 	int lg = u % 10;
 	char ff = '0' + lg;
 	_putchar(ff);
+	
 		return (lg);
 }
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _islower - Entry Point
+ * Return: Always 0
+ * Description: this is a description
+ * @c: also c
+ */
+void jack_bauer(void);
+
+{	
+	int h, m;
+
+	for (h = 0; h >= 23; h++)
+		for (m = 0; m >=59; m++)
+			_putchar(h);
+			_putchar(':');
+			_putchar(m);
+			_putchar('\n')
+}
