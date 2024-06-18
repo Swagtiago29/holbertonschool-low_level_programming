b4c5438c542cefd5082b30a791ba62c853478579
Swagtiago29 <sant29ramos@gmail.com>
2024-06-18 18:13:44 +0000

    ooo

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - Entry point of my code
+* print_alphabet_x10 - Entry point of my code
 *
 * Return: Always 0.
 */
@@ -9,6 +9,7 @@ void print_alphabet_x10(void)
 {
 	char p;
 	int o;
+
 	for (o = 0; o <= 9; o++)
 	{
 		for (p = 'a'; p <= 'z'; p++)
