49dc8164734c60a9d4cbe249b169dc53d01e1247
Swagtiago29 <sant29ramos@gmail.com>
2024-06-18 17:23:52 +0000

    pp

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- *  _islower - Entry Point
+ * jack_bauer - Entry Point
  * Return: Always 0
  * Description: this is a description
- * @c: also c
  */
 void jack_bauer(void)
 
-{	
+{
 	int h, m;
 
 	for (h = 0; h <= 23; h++)
-		for (m = 0; m <=59; m++)
+		for (m = 0; m <= 59; m++)
 {
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
