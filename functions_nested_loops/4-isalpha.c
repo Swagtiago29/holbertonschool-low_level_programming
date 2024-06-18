af6d6b56e7ab0bda99cc22a55a7c41a87a4457c3
Swagtiago29 <sant29ramos@gmail.com>
2024-06-18 00:27:11 +0000

    222

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * main - Entry Point
+ * _isalpha - Entry Point
  * Return: Always 0
  * Description: this is a description
+ * @c: ccc
  */
 int _isalpha(int c)
 {
 	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
-	       	return (1);
+		return (1);
 	else
 		return (0);
 }
