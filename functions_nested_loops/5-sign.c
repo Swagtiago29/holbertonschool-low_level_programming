155d73b71e27cce593568d014fd66a37b0966410
Swagtiago29 <sant29ramos@gmail.com>
2024-06-18 01:17:10 +0000

    nnn

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * main - Entry Point
+ * print_sign - Entry Point
  * Return: Always 0
  * Description: this is a description
+ * @n: nnn
  */
 int print_sign(int n)
 {
-	if (n > '0')	
+	if (n > '0')
 	{
 		_putchar('+');
 		return (1);
