fe748d2b2ff0632cc1bb8e427204ba74c45234c1
Swagtiago29 <sant29ramos@gmail.com>
2024-06-18 13:02:30 +0000

    pp

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _abs - Entry Point
+ * Return: p; -p
+ * Description: this is a description
+ * @p: pp
+ */
+int _abs(int)
+{
+	int p
+	if (p >= '0')
+		return (p)
+	else 
+		return (-p)
+}
