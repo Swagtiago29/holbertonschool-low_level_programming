5a80c18c9568311c9632d9c3cce9b854a6ebb38b
Swagtiago29 <sant29ramos@gmail.com>
2024-06-20 14:40:20 +0000

    222

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strlen - Entry point of my code
+*
+* Return: lenght of array
+* @s: jua s
+*/
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	return (n);
+}
