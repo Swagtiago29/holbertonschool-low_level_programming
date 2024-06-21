e03af62f2160d407b1bd5397f68e234c5948cca8
Swagtiago29 <sant29ramos@gmail.com>
2024-06-21 13:45:02 +0000

    4

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Entry point of my code
+*
+* Return: Always 0.
+* @n: jua n
+*/
+void puts2(char *str)
+{
+	int i
+
+	for (i = 0; str[i] != '\0'; i++ && i++)
+		_putchar(str[i])
+	_putchar('\n')
+}
