09b85c4a966dd02f58401d36af93ad5238a350b7
Swagtiago29 <sant29ramos@gmail.com>
2024-06-21 12:58:59 +0000

    44

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Entry point of my code
+*
+* Return: Always 0.
+* @n: jua n
+*/
+void print_rev(char *s)
+{
+	int i;
+		
+	for(i = 0; s[i] =! '\0'; i++)
+	       ;
+	for(i = 0; s[i] =! '\0'; i--)
+		_putchar(s[i]);
+	_putchar('\n');	
+}
