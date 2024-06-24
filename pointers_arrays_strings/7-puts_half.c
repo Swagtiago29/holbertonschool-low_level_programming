1e18e5b7197571082ab34d090604200d378a3120
Swagtiago29 <sant29ramos@gmail.com>
2024-06-24 13:14:41 +0000

    6

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
-* reset_to_98 - Entry point of my code
+* puts_half - Entry point of my code
 *
 * Return: Always 0.
-* @n: jua n
+* @str: jua str
 */
 void puts_half(char *str)
 {
 	int c = 0, i;
-	
+
 	while (c >= 0)
 	{
 		if (str[c] == '\0')
-			break;	
+			break;
 		c++;
 	}
 	if (c % 2 == 1)
 		i = c / 2;
-	else 
+	else
 		i = (c - 1) / 2;
-	for ( i++ ; i < c; i++)
+	for (i++ ; i < c; i++)
 		_putchar (str[i]);
 	_putchar ('\n');
 }
