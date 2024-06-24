2661c04affcf9c5651b67e6f1a3229a0d9158be6
Swagtiago29 <sant29ramos@gmail.com>
2024-06-24 20:38:54 +0000

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Entry point of my code
+*
+* Return: Always 0.
+* @n: jua n
+*/
+void rev_string(char *s)
+{
+	int i = 0, l = 0;
+	
+	while (s[i]++)
+		l++;
+	for (i = l -1; i >= l / 2; i--)
+		_putchar(s[i]);
+}
