791d3b636a1d164999973eea0a4bbb90de265d46
Swagtiago29 <sant29ramos@gmail.com>
2024-06-24 22:00:32 +0000

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,9 +9,14 @@
 void rev_string(char *s)
 {
 	int i = 0, l = 0;
-
+	char c;
+	
 	while (s[i]++)
 		l++;
 	for (i = l - 1; i >= l / 2; i--)
-		_putchar('c');
+	{
+		c = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = c;
+	}	
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void puts2(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
+int _atoi(char *s);
