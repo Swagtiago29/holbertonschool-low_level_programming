24935c07b565d3adae6fe553fc7f98cdc494e0ed
Swagtiago29 <sant29ramos@gmail.com>
2024-06-24 13:32:27 +0000

    6

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* reset_to_98 - Entry point of my code
+*
+* Return: Always 0.
+* @a: jua a
+*/
+void print_array(int *a, int n)
+{
+	int i
+
+	while (i = 0; i < n; i++)
+		printf (%u, a[n])
+	
+
+}
