d5c8206a58119720d273f75b469af07b2747bb07
Swagtiago29 <sant29ramos@gmail.com>
2024-06-25 22:14:41 +0000

    strncopy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,14 +4,16 @@
 * char *_strncpy - Entry point of my code
 *
 * Return: Always 0.
-* @n: jua n
+* @dest: destino
+* @src: string of chars
+* @n: n?
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int p = 0;
 
 	for (; src[p] != '\0'; p++)
-		if ( p < n)
+		if (p < n)
 			dest[p] = src[p];
 	return (dest);
 }
