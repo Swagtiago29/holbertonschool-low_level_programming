7e92b209c6c1e4d02d1c91d4e09a7c16e4e3f6e7
Swagtiago29 <sant29ramos@gmail.com>
2024-06-25 13:09:32 +0000

    00

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,9 @@ char *_strcat(char *dest, char *src)
 {
 	int i, l = 0;
 	
-	for (i = 0; dest[i] != '\0'; i++)
-		dest[i] = src[l];
-	for (l = 0; src[l] <= '\0'; l++);	
+	for (i = 0; dest[i] != '\0'; i++);
+	dest[i] = src[l];
+	for (; src[l] != '\0'; l++);
+			
 	return (dest);	
 }
