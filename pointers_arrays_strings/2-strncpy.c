2f5797f6c94a9e878e5423ba5dd9205f2d032412
Swagtiago29 <sant29ramos@gmail.com>
2024-06-25 15:00:49 +0000

    00

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,8 +11,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int l = 0, p = 0;
 
 	for (; src[p] != '\0'; p++)
+	{
 		l++;
-	for (; l <= n; p++)
+		for (; l <= n; p++)
 		dest[p] = src[p];
+	}	
 	return (dest);
 }
