1ed4631695b36c70184517f91edfa96a70386e92
Swagtiago29 <sant29ramos@gmail.com>
2024-06-25 01:58:40 +0000

    5

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Entry point of my code
+*
+* Return: Always 0.
+* @n: jua n
+*/
+char *_strcat(char *dest, char *src);
+{
+	
+}
---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _atoi - Main, entry point
+*
+* Return: Always 0.
+* @s: ss ร.รณ
+*/
+int _atoi(char *s)
+{
+	
+}
