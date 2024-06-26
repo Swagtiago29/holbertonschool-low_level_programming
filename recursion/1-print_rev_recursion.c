30e8985e91e433e38512c56849990f9a36f14edc
Swagtiago29 <sant29ramos@gmail.com>
2024-06-26 18:07:22 +0000

    1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - Entry point of my code
+*
+* Return: Always 0.
+* @n: jua n
+*/
+void _print_rev_recursion(char *s);
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(s);
+		s--;
+	}
+}
