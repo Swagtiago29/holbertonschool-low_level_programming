9c679571f26cc7e8ed7efff6b7108f754ef3c99e
Swagtiago29 <sant29ramos@gmail.com>
2024-06-26 00:18:54 +0000

    strncopy

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] = '\0'; i++)
+	for (i = 0; s1[i] == '\0'; i++)
 	       	if (s1[i] == s2[i])
 			return (0);
-		if else (s1[i] < s2[i])
+		else if (s1[i] < s2[i])
 			return (-15);
 		else 
 			return (15);
+	return (0);
 }
