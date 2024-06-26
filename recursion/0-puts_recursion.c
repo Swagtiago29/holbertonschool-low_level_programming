81f4035fd679cbdb834ba83e6e28d0256d7f4a86
Swagtiago29 <sant29ramos@gmail.com>
2024-06-26 13:55:06 +0000

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,12 +8,10 @@
 */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	if (i != '\0')	
+	if (*s != '\0')	
 	{
-		_putchar(s[i]);
-		_puts_recursion(i + 1);
+		_putchar(*s);
+		_puts_recursion(*s + 1);
 	}
 	return (0);
 }
