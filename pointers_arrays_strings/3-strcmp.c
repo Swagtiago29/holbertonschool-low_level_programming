57a7f1b7c90ab3b08cf98f668534ca50e88c74f7
Swagtiago29 <sant29ramos@gmail.com>
2024-06-26 01:58:47 +0000

    strncopy

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,20 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
-* char *_strncpy - Entry point of my code
+* _strcmp - Entry point of my code
 *
 * Return: Always 0.
-* @dest: destino
-* @src: string of chars
-* @n: n?
+* @s1: destino
+* @s2: lolol
 */
 int _strcmp(char *s1, char *s2)
 {
 	int i, l1, l2;
-	
+
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
 	for (i = 0; i < l1 && i < l2; i++)
-	{	
+	{
 		if (s1[i] < s2[i])
 			return (-13);
 		else if (s1[i] > s2[i])
