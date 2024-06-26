41d1a9d3dd52571d31cd4486281b37f685b12f39
Swagtiago29 <sant29ramos@gmail.com>
2024-06-26 00:41:05 +0000

    strncopy

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "2-strlen.c"
 /**
 * char *_strncpy - Entry point of my code
 *
@@ -10,10 +10,12 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] == '\0'; i++)
-	{
+	int i, l1, l2;
+	
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	for (i = 0; i < l1 && i < l2; i++)
+	{	
 		if (s1[i] < s2[i])
 			return (-13);
 		else if (s1[i] > s2[i])
