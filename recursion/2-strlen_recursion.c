d660e1b00d86aedb899a50a7a91ef5360229144f
Swagtiago29 <sant29ramos@gmail.com>
2024-06-27 22:25:44 +0000

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,13 +6,13 @@
 * Return: Always 0.
 * @s: bruh.
 */
-void _puts_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{	
-		s++;
-		return (s);
-		_puts_recursion(s);		
+		return (*s);
+		_puts_recursion(s + 1);		
 	}
-	return (0);
+	else 
+		return (0);
 }
