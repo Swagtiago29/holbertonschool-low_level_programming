339bd67e8a559267ac03b6d07558eb4759a85d9d
Swagtiago29 <sant29ramos@gmail.com>
2024-06-27 23:29:15 +0000

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,17 +2,17 @@
 
 /**
 * _puts_recursion - Entry point of my code
-*
 * Return: Always 0.
 * @s: bruh.
 */
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
-	{	
+	{
+		s++;
 		return (*s);
-		_puts_recursion(s + 1);		
+		_puts_recursion(s);
 	}
-	else 
+	else
 		return (0);
 }
