ac8cc39bf740eb6adb036c3d7617a7d7573ec213
Swagtiago29 <sant29ramos@gmail.com>
2024-06-28 13:50:00 +0000

    3

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - Entry point of my code
+* Return: Always 0.
+* @s: bruh.
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y >= 0)
+	
+		return (x * _pow_recursion(x, y - 1));
+	else 
+		return (-1);
+}
