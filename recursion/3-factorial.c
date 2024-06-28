e7c96cb829d53b28082868f4868f39b720064da9
Swagtiago29 <sant29ramos@gmail.com>
2024-06-28 12:31:59 +0000

    3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - Entry point of my code
+* Return: Always 0.
+* @s: bruh.
+*/
+int factorial(int n)
+{
+	if (n >= 1)
+		n * n;
+		return(factorial(n - 1));
+	else return (-1);
