eba5a6cd722b82c1b8bda3f3939a0838f5e467e8
Swagtiago29 <Sant29ramos@gmail.com>
2024-06-28 17:16:23 -0300

    Update 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,18 +5,21 @@
 * Return: Always 0.
 * @s: bruh.
 */
-int pepinillo(int x)
-{	
-	if (x != _sqrt_recursion(x) * _sqrt_recursion(x))
-		return (2);
-	else
-	       	return (-1);
+int pepinillo(int x, int o, int res)
+{
+        if (res >= x)
+		res = pepinillo(x, o + 1, res);
+		return (
+        else
+                return (-1);
 }
 
 int _sqrt_recursion(int n)
-{	
-	if (n >= 0) 
-		return (pepinillo(n));
-	else
-		return (-1);
+{
+        int o = 1, res = 0;
+
+        if (n >= 0)
+                return (pepinillo(n, o, res));
+        else
+                return (-1);
 }
