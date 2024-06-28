9d0c7af95dee46b4dc5a7b5a18c983e960342ea5
Swagtiago29 <sant29ramos@gmail.com>
2024-06-28 19:21:25 +0000

    555

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,14 +8,14 @@
 int pepinillo(int x)
 {	
 	if (x >= 0)
-		return (pepinillo(x + 1) * pepinillo(x + 1));
+		return (pepinillo(x + 1));
 	else
 	       	return (-1);
 }
 
 int _sqrt_recursion(int n)
 {	
-	if (n == pepinillo(n))
+	if (n == pepinillo(n) * pepinillo(n))
 		return (pepinillo(n));
 	else
 		return (-1);
