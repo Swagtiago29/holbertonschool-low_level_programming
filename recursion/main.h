941fa62a00c5706e4645812916475e7a4e7936b6
Swagtiago29 <Sant29ramos@gmail.com>
2024-06-28 17:55:02 -0300

    Update main.h

--- recursion/main.h
+++ recursion/main.h
@@ -4,4 +4,4 @@ void _print_rev_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int pepinillo(int x, int o, int res);
+int pepinillo(int x, int o);
