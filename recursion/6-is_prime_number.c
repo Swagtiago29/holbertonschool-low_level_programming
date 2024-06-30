813c093052423ea584cd514ae8326c3a03ad5dca
Swagtiago29 <sant29ramos@gmail.com>
2024-06-30 14:49:32 +0000

    6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* pepinillo - entry function for recursion
+* Return: o
+* @x: bruh.
+* @o: oooooooooo
+* return: pepinillinnn
+*/
+int pepinillo(int n, int i)
+{
+	if (n % i != 0)
+		return (primito(n, i + 1));
+	else if (n * 1 == x)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: variable
+ * Return: alguna cosa
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n >= 0)
+		return (primito(n, i));
+	else
+		return (0);
+}
