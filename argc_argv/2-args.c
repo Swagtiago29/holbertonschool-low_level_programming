932098764cf652d0481bab067bc0c8b3732e14ed
Swagtiago29 <sant29ramos@gmail.com>
2024-06-30 17:15:51 +0000

    000

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -12,6 +12,6 @@ int main(int argc __attribute__((unused)), char *argv[])
 {
 	int i = 0;
 	
-	for (i >= 0; argv[i] == '\0'; i++)
+	for (i >= 0; *argv[i] == '\0'; i++)
 		(printf("%s\n", argv[i]);
 	return (0);
