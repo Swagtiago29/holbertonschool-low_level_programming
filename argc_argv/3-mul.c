52f4605a2136e2b907f8a4706d52369c4d4c8e34
Swagtiago29 <sant29ramos@gmail.com>
2024-06-30 17:33:51 +0000

    000

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0.
+* @argc: int #strings
+* @argv: array de strings
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	if (argc = 3)
+		printf("%s\n", argv[1]) * argv[2]);
+	else 
+		printf("Error");
+	return (0);
+}
