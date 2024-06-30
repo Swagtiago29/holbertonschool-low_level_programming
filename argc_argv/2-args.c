d1268b02db5bf3dd5b97c73b4ab83bf8b6ce1055
Swagtiago29 <sant29ramos@gmail.com>
2024-06-30 17:11:27 +0000

    000

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0.
+* @argc: int #strings
+* @argv: array de strings
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i = 0;
+	
+	for (i >= 0, i == '\0', i++)
+		(printf("%s\n", argv[i]);
+	return (0);
