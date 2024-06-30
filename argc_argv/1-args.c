523989df789343b3495c764f4b3195bc8bf8ec1d
Swagtiago29 <sant29ramos@gmail.com>
2024-06-30 16:07:13 +0000

    000

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0.
+* @argc: int #strings
+* @argv: array de strings
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
