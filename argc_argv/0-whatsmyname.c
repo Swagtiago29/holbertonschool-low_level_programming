ae4789ead24bbf9f66d3d2033386e692054be93c
Swagtiago29 <sant29ramos@gmail.com>
2024-06-30 15:35:12 +0000

    000

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts_recursion - Entry point of my code
+*
+* Return: Always 0.
+* @s: bruh.
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv[0])
+	ignore(argc);
+	return (0);
+}
