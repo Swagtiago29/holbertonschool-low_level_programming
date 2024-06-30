6115949ab4b236151c01c42fb20f1da9564a51a2
Swagtiago29 <sant29ramos@gmail.com>
2024-06-30 21:35:26 +0000

    000

---
+++ argc_argv/4-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0.
+* @argc: int #strings
+* @argv: array de strings
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int e = 0 + atoi(argv[i]);
+
+	for (i = 0; argv[i] < argc; i++)
+		printf(%d, e);
+	return (0);
+}
