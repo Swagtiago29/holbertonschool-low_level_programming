9248c665b095464663a0c74836d1cb76b840149c
Swagtiago29 <sant29ramos@gmail.com>
2024-06-30 17:35:42 +0000

    000

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,10 +8,10 @@
 * @argc: int #strings
 * @argv: array de strings
 */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	if (argc = 3)
-		printf("%s\n", argv[1]) * argv[2]);
+	if (argc == 3)
+		printf("%s\n", *argv[1] * *argv[2]);
 	else 
 		printf("Error");
 	return (0);
