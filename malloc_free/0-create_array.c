2c143486ca9a5c58f844b284d03b2ece3d7016f3
Swagtiago29 <sant29ramos@gmail.com>
2024-07-02 14:24:26 +0000

    oo

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -20,9 +20,9 @@ char *create_array(unsigned int size, char c)
 	{
 		s = malloc(size * sizeof(c));
 		for (i = 0; i < size; i++)
-		{
-			*s =c;
-			return(*s);
-		}
+			{
+			*s = c;
+			return(s);
+			}
 	}	
 }
