aae343e234355941009543c0439abf802e7c9b52
Swagtiago29 <sant29ramos@gmail.com>
2024-07-02 18:06:01 +0000

    oo

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,12 +12,13 @@
 char *_strdup(char *str)
 {
 	char *cc;
-	unsigned int i;
+	unsigned int i = 0;
 	unsigned int o;
 
-	if (str == NULL || str == NULL)
+	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
+		i++
 	cc = malloc((i + 1) * sizeof(char));
 	if (cc == NULL)
                 return (NULL);
