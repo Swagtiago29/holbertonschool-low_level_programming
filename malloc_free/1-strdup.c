e8a5dfea6170f6bfac33b35ddbf5e71d6c2fcb77
Swagtiago29 <sant29ramos@gmail.com>
2024-07-02 16:23:40 +0000

    oo

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib>
+
+/**
+* main - Entry point
+*
+* Return: Always 0.
+* @argc: int #strings
+* @argv: array de strings
+*/
+char *_strdup(char *str)
+{
+	char *cc;
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+		cc[i] = str[i];
+	cc = malloc(i * sizeof(str))
+	return (cc);
+}
