7731d7a2de7919bda2fdfa811126c383841695d7
Swagtiago29 <sant29ramos@gmail.com>
2024-07-04 15:45:04 +0000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,48 +6,47 @@
 * str_concat - Entry point
 *
 * Return: Always 0.
-* @size: size of array
-* @c: charrr
+* @s1: array 1
+* @s2: array 2
 */
 char *str_concat(char *s1, char *s2)
 {
-        unsigned int i = 0;
-        unsigned int o = 0;
-        unsigned int len = 0;
-        unsigned int p = 0;
+	unsigned int i = 0;
+	unsigned int o = 0;
+	unsigned int len = 0;
+	unsigned int p = 0;
 	char *s3;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i] != '\0')
 	{
 		i++;
 		len++;
 	}
 	i = 0;
-        while (s2[i] != '\0')
+	while (s2[i] != '\0')
 	{
-  		i++;
-  		len++;
+		i++;
+		len++;
 	}
-        s3 = malloc((len + 1) * sizeof(char));
+	s3 = malloc((len + 1) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
-
 	while (s1[o] != '\0')
 	{
 		s3[o] = s1[o];
 		o++;
 	}
-        while (s2[p] != '\0')
+	while (s2[p] != '\0')
 	{
 		s3[o] = s2[p];
 		o++;
 		p++;
 	}
-        if (s2[p] == '\0')
-                s3[o] = '\0';
-        return (s3);
+	if (s2[p] == '\0')
+		s3[o] = '\0';
+	return (s3);
 }
