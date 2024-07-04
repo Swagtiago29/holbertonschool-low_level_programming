8f6fc6ca79b5a80702aa4f97806e4656d2bccc4b
Swagtiago29 <sant29ramos@gmail.com>
2024-07-04 17:49:52 +0000

    3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* alloc_grid - Entry point
+*
+* Return: Always 0.
+* @width: width of grid?
+* @height: height of grid?
+*/
+int **alloc_grid(int width, int height)
+{
+	char **s;
+	int i = 0;
+	int o = 0;
+	
+	if (width <= 0)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+	s = malloc(width * sizeof(int *))
+	if (s == NULL)
+		return (NULL);
+
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 char *_strdup(char *str);
+int **alloc_grid(int width, int height);
