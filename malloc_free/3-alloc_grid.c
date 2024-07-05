9200c411f4b9706a5dcc3659cba52ab8a3f2f983
Swagtiago29 <sant29ramos@gmail.com>
2024-07-05 17:05:38 +0000

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,27 +14,27 @@ int **alloc_grid(int width, int height)
 	int **s;
 	int i = 0;
 	int o = 0;
-	
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	s = malloc(height * sizeof(int *));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
-	{	
+	{
 		s[i] = malloc(width * sizeof(int));
 		if (s[i] == NULL)
 		{
 			for (; i >= 0; i--)
-                		free(s[i]);	
-            		free(s);
-            		return (NULL);
+				free(s[i]);
+			free(s);
+			return (NULL);
 		}
-	}	
+	}
 	for (i = 0; i < height; i++)
-	{	
+	{
 		for (o = 0; o < width; o++)
 			s[i][o] = 0;
 	}
-	return(s);
+	return (s);
 }
