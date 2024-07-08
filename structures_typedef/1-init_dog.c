5640a178f050906f56f8cf2c545020538e4d6912
Swagtiago29 <sant29ramos@gmail.com>
2024-07-08 19:08:11 +0000

    0

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,12 +8,17 @@
  *
  *
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-typedef struct dog 
+struct dog *d;
 {
 char *name;
 float age;
 char *owner;
-} dog;
+}
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog *d;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
