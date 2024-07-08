e61154e9cde6a08f84ef1d29eb42ee4a2c083c94
Swagtiago29 <sant29ramos@gmail.com>
2024-07-08 18:10:03 +0000

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,7 +2,12 @@
 #define DOG_H
 
 #include <stdio.h>
-
+/**
+ * struct dog - info general del perro
+ * @name: nombre del rope
+ * @age: edad del rope
+ * @owner: due√o del rope
+ */
 struct dog
 {
 char *name;
