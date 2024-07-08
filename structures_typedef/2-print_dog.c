d7901d091bb946e77fa54931158aef5252c4960b
Swagtiago29 <sant29ramos@gmail.com>
2024-07-08 19:52:47 +0000

    0

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *init_dog - perris
+ *return: boid
+ *@name: nombre
+ *@owner: dueÃo
+ *@age: edad
+ *@d: struct on dog.h
+ */
+void print_dog(struct dog *d)
+{
+printf("Name: "%s'\n'"Age: "%.6f'\n'"Owner: "%s'\n', my_dog.name, my_dog.age, my_dog.owner)
+
+}
