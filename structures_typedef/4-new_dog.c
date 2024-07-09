bfdabb66e5b93f115723cefd81d0be33e9f3046b
Swagtiago29 <sant29ramos@gmail.com>
2024-07-09 14:46:22 +0000

    3

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -20,8 +20,9 @@ if (nuevi_perri == NULL)
 	free (nuevi_perri);
 	return (NULL);
 }
-nuevi_perri->name = name;
+nuevi_perri->name = malloc(_strlen(name) + 1);
 nuevi_perri->age = age;
-nuevi_perri->owner = owner;
+nuevi_perri->owner = malloc(_strlen(owner) + 1);
+free(nuevi_perri);
 return (nuevi_perri);
 }
