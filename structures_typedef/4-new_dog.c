80ed1801f61d3665e3103dbfd64859e9fd0cb9ab
Swagtiago29 <sant29ramos@gmail.com>
2024-07-09 13:52:44 +0000

    3

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *nuevi_perri;
 
-nuevi_perri = malloc(sizeof(struct dog_t));
+nuevi_perri = malloc(sizeof(dog_t));
 if (nuevi_perri == NULL)
 	return(NULL);
 nuevi_perri->name = name;
