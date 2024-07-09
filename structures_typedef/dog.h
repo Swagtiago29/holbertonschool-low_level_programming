6dce78c24876554a9b4d1dc91a332d46a71d001a
Swagtiago29 <sant29ramos@gmail.com>
2024-07-09 13:23:17 +0000

    3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @age: edad del rope
  * @owner: due√o del rope
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
