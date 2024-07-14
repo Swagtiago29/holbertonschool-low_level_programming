b61e39444c5d145ef6c716bee67629cc76da7f9e
Swagtiago29 <sant29ramos@gmail.com>
2024-07-14 22:34:59 +0000

    o

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+* print_name - Entry point
+*
+* Return: void
+* @f: function to point
+* @name: name to print
+*/
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	int i;
+
+	for (i = 0; i <= size; i++)
+		_pitchar(array[i]);
+}
