9d46971e41eb4edcb3147ed12ec48f1b9c4a35fa
Swagtiago29 <sant29ramos@gmail.com>
2024-07-14 23:41:19 +0000

    o

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+* print_name - Entry point
+*
+* Return: void
+* @f: function to point
+* @name: name to print
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int (*sea)(int) = cmp
+
+	for (i = 0; i < size; i++)
+		cmp (array[i]);
+	return (array[i]);
+
+}
