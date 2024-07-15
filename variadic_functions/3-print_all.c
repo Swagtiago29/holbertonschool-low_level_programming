cd1c04b59a9d4bbfb30af6c5dd9da91847e0e542
Swagtiago29 <sant29ramos@gmail.com>
2024-07-15 16:24:16 +0000

    comm

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+* sum_them_all - Entry point
+*
+* Return: pp
+* @n: number of args
+*/
+void print_all(const char * const format, ...);
+{
+	int i = 0;
+	int l = 0;
+
+	while (format[l] != '\0')
+		{
+		l++;
+		
+		}
+}
