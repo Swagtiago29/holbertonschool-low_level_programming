367ccda313da3df5bf0cc96068915d5f68684aef
Swagtiago29 <sant29ramos@gmail.com>
2024-07-15 02:12:31 +0000

    o

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+* sum_them_all - Entry point
+*
+* Return: pp
+* @n: number of args
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int p;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		if (separator != NULL)
+			prtintf("%i%s", va_arg(args, int);
+		else
+			printf("%i", va_arg(args, int));
+	printf("\n)
+}
