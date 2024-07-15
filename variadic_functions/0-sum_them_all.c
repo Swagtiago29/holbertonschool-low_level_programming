133d44b8b882cddb2c930e3bd8b7652a0dc17b5f
Swagtiago29 <sant29ramos@gmail.com>
2024-07-15 01:54:21 +0000

    o

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "variadic_function.h"
+#include <stdlib.h>
+
+/**
+* print_name - Entry point
+*
+* Return: void
+* @f: function to point
+* @name: name to print
+*/
+int sum_them_all(const unsigned int n, ...);
+{
+	va_list args;
+	int i;
+	int p;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		p = p va_arg(args, int);
+	va_end(args);
+	return(p);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_H
+#define FUNTION_H
+
+int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
+
+#endif
