8f842aef6e5d609c5e1f31f65283e2b2fc253064
Swagtiago29 <sant29ramos@gmail.com>
2024-07-15 03:02:19 +0000

    o

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (va_arg(args, char *) != NULL)
 			printf("%s", va_arg(args, char *));
 		if (separator != NULL && i < n -1)
-			printf("%s%s", va_arg(args, char *), separator);
+			printf("%s", separator);
 		}
 	printf("\n");
 	va_end(args);
