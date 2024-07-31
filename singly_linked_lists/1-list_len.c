6e50a7721e3faed8f3199c54cf4c8f8aa8e7246c
Swagtiago29 <sant29ramos@gmail.com>
2024-07-31 21:46:25 +0000

    add 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_list - listita
+ * Return: number of nodes
+ * @h: hhhhhhh
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		printf("[%u]\n", h->len);
+		h = h->next;
+		
+	}
+	return (i);
+}
