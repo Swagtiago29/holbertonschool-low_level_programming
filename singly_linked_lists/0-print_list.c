34f07be19285af9de6ecaf62d1b7683d6fe91505
Swagtiago29 <sant29ramos@gmail.com>
2024-07-31 17:28:17 +0000

    0 thingy uncomplete

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "lists.h"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *print_list - listita
+ *return: number or nodes
+ *@name: nombre
+ *@owner: dueÃo
+ *@age: edad
+ *@d: struct on dog.h
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		if (h->str != NULL)
+		{	
+			printf("[0] (nil)");
+			return (i);
+		}
+		else 
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}
+	return (i);
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+hola esto es un proyecto sobre linked lists
