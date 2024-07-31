5df18bed7abe7057ee66d6d771ce2f678c325b12
Swagtiago29 <sant29ramos@gmail.com>
2024-07-31 21:55:48 +0000

    2 incomplete

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_list - listita
+ * Return: number of nodes
+ * @h: hhhhhhh
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t liston
+
+	while (head != NULL)
+	{
+		i++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}
+	return (i);
+}
