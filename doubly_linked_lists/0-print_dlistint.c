eb77ad9bccc577efa7e84eda610d4399b37f964d
Swagtiago29 <sant29ramos@gmail.com>
2024-08-02 21:57:22 +0000

    added task 1, lists.h

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - listita
+ * Return: number of nodes
+ * @h: hhhhhhh
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		printf("[%u]\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
