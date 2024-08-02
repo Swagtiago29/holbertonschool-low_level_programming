30a67b8d7da2d8a482cf76a9ef67c9c60d8812a7
Swagtiago29 <sant29ramos@gmail.com>
2024-08-02 22:03:36 +0000

    fix 0?

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -14,7 +14,10 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		i++;
-		printf("%u\n", h->n);
+		if (n < '0')
+			printf("-%u\n", h->n);
+		else
+			printf("%u\n", h->n);
 		h = h->next;
 	}
 	return (i);
