dd40102f82ff4e977a2e8848fdc92626993e19b0
Swagtiago2 <sant29ramos@gmail.com>
2024-08-04 22:21:17 +0000

    added directory, file, readme, also test 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ * Description: alguna descripcion coherente
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int n, o;
+	char *buf;
+	ssize_t i;
+
+	if(filename == NULL)
+		return (0);
+	n = open(filename, O_RDONLY);
+	if (n == -1)
+		return (0);
+	o = read(0, buf,letters);
+	i = write (1, buf, letters);
+	return (i);
+	
+}
