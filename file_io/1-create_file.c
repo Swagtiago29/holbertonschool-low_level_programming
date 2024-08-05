d0ae34d8a4f211c05f7a3a8f0cfbd07195d68c65
Swagtiago2 <sant29ramos@gmail.com>
2024-08-05 00:18:22 +0000

    task 1 test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w;
 
-	fd =  open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSUR);
+	fd =  open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
